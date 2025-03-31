1714e6b22bdd600e286691c657029459b09c711c
Ben Hoff <hoff.benjamin.k@gmail.com>
2025-03-31 15:28:39 -0400

    remove phising

--- teams_breaker/teams_breaker.py
+++ teams_breaker/teams_breaker.py
@@ -87,93 +87,12 @@ def build_argparser() -> argparse.ArgumentParser:
         action="store_true",
         help="Run in dry run self mode. Print status",
     )
-    parser.add_argument(
-        "--send",
-        dest="send",
-        required=False,
-        default=False,
-        action="store_true",
-        help="WARNING: Run in send mode. Send target users phishing links.",
-    )
     return parser
 
 
 def create_thread_by_method(method):
     sw = {"closed_chat": teams_api.chat_create_closed_chat, "meeting": teams_api.chat_create_meeting}
     return sw.get(method)
-
-
-def send_phish(
-    bearer_token,
-    skype_token,
-    sender_sharepoint_url,
-    sender_drive,
-    sender_info,
-    chat_title,
-    u,
-    template,
-    preview=True,
-    interactive=True,
-    upload_info=None,
-    method="closed_chat",
-):
-    if interactive:
-        inp = input(f"Sending phish to {u}, are you sure? (N/y): ")
-        if inp not in ["y", "Y"]:
-            logger.info("User did not confirm. Leaving...")
-            sys.exit(0)
-
-    ustatus = TeamsUser(bearer_token, u).get_status() if sender_info.get("email") != u else sender_info
-
-    if not ustatus:
-        logger.warning(f"Could not get status for user: {u}. Skipping...")
-        return False
-
-    if not ustatus.get("displayName"):
-        logger.warning(f"Could not get displayName for user: {u}. Skipping...")
-        return False
-
-    ustatus["displayName"] = ustatus["displayName"].title()
-
-    logger.info(f"Creating chat with {ustatus.get('displayName')}, {chat_title} as name")
-
-    # method = "closed_chat"
-
-    # thread = (
-    #    teams_api.chat_create_meeting(bearer_token, skype_token, sender_info, sender_info, chat_title)
-    #    if preview
-    #    else teams_api.chat_create_meeting(bearer_token, skype_token, sender_info, ustatus, chat_title)
-    # )
-
-    if upload_info:
-        invite_info = teams_api.file_send_invite(
-            sharepoint_token, sender_sharepoint_url, sender_drive, sender_info, ustatus, upload_info
-        )
-
-    thread = (
-        create_thread_by_method(method)(bearer_token, skype_token, sender_info, sender_info, chat_title)
-        if preview
-        else create_thread_by_method(method)(bearer_token, skype_token, sender_info, ustatus, chat_title)
-    )
-
-    message = convert_str_to_html(chevron.render(template, ustatus))
-
-    if upload_info:
-        if code := teams_api.chat_send_msg_with_file(
-            skype_token, thread, message, sender_sharepoint_url, sender_drive, upload_info, *invite_info
-        ):
-            logger.success(f"Message with file sent to {u}")
-            return True
-        else:
-            logger.error(f"Could not send message with file to {u}, error: {code}")
-            return False
-    else:
-        if code := teams_api.chat_send_msg(skype_token, thread, message):
-            logger.success(f"Message sent to {u}")
-            return True
-        else:
-            logger.error(f"Could not send message to {u}, error: {code}")
-            return False
 
 
 def print_users_status(bearer_token, users):
@@ -227,36 +146,11 @@ if __name__ == "__main__":
         logger.error("Configuration file parameter needed when sending.")
         sys.exit(1)
 
-    if args.attachment and configuration.get("attachment"):
-        logger.error(
-            "Cannot set both `attachment` command line argument (--attachment) and `attachment` in .yaml configuration"
-            " file. Use only one."
-        )
-        sys.exit(1)
-
-    if args.send:
-        logger.warning("Sending to victims")
-        inp = input("Sending to real victims, are you sure? (N/y): ")
-        if inp not in ["y", "Y"]:
-            logger.info("User did not confirm. Leaving...")
-            sys.exit(0)
-    if args.dry_run_self:
-        logger.warning("Running in dry run self mode.")
-        inp = input("Running in dry run self mode, are you sure? (N/y): ")
-        if inp not in ["y", "Y"]:
-            logger.info("User did not confirm. Leaving...")
-            sys.exit(0)
-
     if args.configuration and args.list:
         logger.error("Cannot use both configuration file and list parameters at the same time.")
         sys.exit(1)
 
     to_upload = None
-
-    if args.attachment:
-        to_upload = args.attachment
-    if configuration.get("attachment"):
-        to_upload = configuration["attachment"]
 
     users = None
     list_path = None
@@ -285,37 +179,6 @@ if __name__ == "__main__":
 
     if args.enum_users:
         print_users_status(bearer_token, users)
-        sys.exit(0)
-
-    if args.dry_run_self and configuration:
-        total = len(users)
-        successes = 0
-        upload_info = None
-        if to_upload:
-            upload_info = teams_api.file_upload(
-                sharepoint_token, sender_sharepoint_url, sender_drive, to_upload, sender_info
-            )
-        for i, u in enumerate(users):
-            logger.info(f"Sending message to {u} ({i+1}/{total})")
-            template = configuration["message"]
-            time.sleep(1)
-            if send_phish(
-                bearer_token,
-                skype_token,
-                sender_sharepoint_url,
-                sender_drive,
-                sender_info,
-                configuration["chat_title"],
-                u,
-                template,
-                preview=True,
-                upload_info=upload_info,
-                method=method,
-                interactive=args.interactive,
-            ):
-                successes += 1
-        logger.info(f"Sent {successes}/{total} messages succesfully!")
-
         sys.exit(0)
 
     if args.dry_run and configuration:
@@ -352,6 +215,7 @@ if __name__ == "__main__":
         successes = 0
 
         upload_info = None
+        """
         if to_upload:
             upload_info = teams_api.file_upload(
                 sharepoint_token, sender_sharepoint_url, sender_drive, to_upload, sender_info
@@ -379,3 +243,4 @@ if __name__ == "__main__":
         logger.info(f"Sent {successes}/{total} messages succesfully!")
 
         sys.exit(0)
+        """
